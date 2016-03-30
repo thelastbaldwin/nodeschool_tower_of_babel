a2a325571c51b33a0174970e3ef46dc10b8c1f1c
Steve Minor <stv.mnr@gmail.com>
2016-03-29 20:44:20 -0700

    initial commit

---
+++ babel_setup.js
@@ -0,0 +1,3 @@
+var arg = process.argv[2];
+
+console.log(`Hello ${arg}`);
---
+++ modules_with_name_executable.js
@@ -0,0 +1,8 @@
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+import {PI, sqrt, square} from './modules_with_name_module.js'
+
+console.log(PI);
+console.log(sqrt(+arg1));
+console.log(square(+arg2));
